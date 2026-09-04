842f7916a353c55e5411fe0d8ac046dbc32eaa3c
demoniman0-pixel <demoniman0@gmail.com>
2026-09-05 00:13:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const units = [
   { name: "Snake", value: "100,000", demand: "Medium", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Snake" },
 
   // --- Скриншот 1: Верхний ряд (Mythic) ---
-  { name: "Shinobi", value: "70,000", demand: "High", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Shinobi" },
-  { name: "Yeti", value: "70,000", demand: "Medium", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Yeti" },
-  { name: "Builder", value: "70,000", demand: "Low", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Builder" },
+  { name: "Shinobi", value: "70,000", demand: "High", rarity: "Mythic", image: "https://i.ibb.co/G3dsk01M/IMG-20260905-000813.jpg" },
+  { name: "Yeti", value: "70,000", demand: "Medium", rarity: "Mythic", image: "https://i.ibb.co/4wpf6mqq/IMG-20260905-000705.jpg" },
+  { name: "Builder", value: "70,000", demand: "Low", rarity: "Mythic", image: "https://i.ibb.co/tpxcDGhr/IMG-20260905-000411.jpg" },
   { name: "Goblin Wizard", value: "70,000", demand: "High", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Goblin+Wizard" },
   { name: "Dark Jonin", value: "70,000", demand: "Insane", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Dark+Jonin" },
   { name: "Ghost Pirate", value: "70,000", demand: "High", rarity: "Mythic", image: "https://placehold.co/200x200/1e1e2d/ef4444?text=Ghost+Pirate" },
